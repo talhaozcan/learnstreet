a0228cd8cfda51cab627f04199be693f4a880792
ahmet talha özcan <a.talhaozcan@gmail.com>
2014-06-02 04:24:32 -0700

    happy_numbers commit

---
+++ Happy Numbers/happy_numbers.py
@@ -0,0 +1,13 @@
+def happynum(num):
+    while True:
+        if len(str(num)) != 1:          
+            temp=0
+            for i in range(0,len(str(num))):
+                temp = temp+ (int(str(num)[i]))**2
+            num = temp
+        else:
+            break
+    if int(num) == 1 :
+        return 'Happy Number'
+    else:
+        return 'Unhappy Number'    
